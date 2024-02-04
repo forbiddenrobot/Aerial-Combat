9cf7176aa521e5330c269d233508df0ce738d1fc
ForbiddenRobot <45095682+forbiddenrobot@users.noreply.github.com>
2024-02-04 08:08:28 -0800

    Level 2 Boss Added & Green Enemy
    Added a boss for level 2. Also added a green enemy. Green Enemy has six health and 2.5 speed.

---
+++ Space Shooter/Assets/Scripts/L2Boss.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2Boss : MonoBehaviour
+{
+    [Header("Shooting Stuff")]
+    [SerializeField] private float wingWeaponCooldown;
+    [SerializeField] private float wingWeaponDamage;
+    [SerializeField] private float[] wingWeaponRotations;
+    private int currentWingRotationIndex = 0;
+
+    [Header("Fill Out Once")]
+    [SerializeField] private List<Transform> firepointWings;
+    [SerializeField] private GameObject wingMissle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("WingAttack", 2f, wingWeaponCooldown);
+    }
+
+    private void WingAttack()
+    {
+        foreach (Transform firepoint in firepointWings)
+        {
+            if (firepoint == null)
+            {
+                return;
+            }
+
+            GameObject missle = Instantiate(wingMissle, firepoint.position, Quaternion.Euler(0, 0, wingWeaponRotations[currentWingRotationIndex]));
+            EnemyBullet missleScript = missle.GetComponent<EnemyBullet>();
+            missleScript.damage = wingWeaponDamage;
+        }
+
+        currentWingRotationIndex = (currentWingRotationIndex + 1) % wingWeaponRotations.Length;
+
+    }
+}
